96b40f848e5e6b18c37df62b9d63b5dc82cce4fa
yuichi-systembase <yuichi@systembase.co.jp>
2013-05-20 15:08:56 +0900

    PDFの出力方法を変更
    画像出力から直接出力へ変更

--- src.pdf/jp/co/systembase/report/renderer/pdf/elementrenderer/BarcodeRenderer.java
+++ src.pdf/jp/co/systembase/report/renderer/pdf/elementrenderer/BarcodeRenderer.java
@@ -3,8 +3,11 @@ package jp.co.systembase.report.renderer.pdf.elementrenderer;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
+import jp.co.systembase.barcode.Barcode.BarContent;
 import jp.co.systembase.barcode.ITF;
 import jp.co.systembase.barcode.YubinCustomer;
 import jp.co.systembase.core.Cast;
@@ -19,12 +22,15 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
 import com.lowagie.text.Image;
 import com.lowagie.text.pdf.Barcode;
 import com.lowagie.text.pdf.Barcode128;
 import com.lowagie.text.pdf.Barcode39;
 import com.lowagie.text.pdf.BarcodeCodabar;
 import com.lowagie.text.pdf.BarcodeEAN;
+import com.lowagie.text.pdf.BaseFont;
 import com.lowagie.text.pdf.PdfContentByte;
 import com.lowagie.text.pdf.PdfTemplate;
 
@@ -44,6 +50,7 @@ public class BarcodeRenderer implements IElementRenderer {
 		Image image = null;
 		PdfContentByte cb = renderer.writer.getDirectContent();
 		String type = (String)design.get("barcode_type");
+		float scaleMargin = 2.0f;
 		try{
 			if (type != null && type.equals("code39")){
 				Barcode39 barcode = new Barcode39();
@@ -129,31 +136,63 @@ public class BarcodeRenderer implements IElementRenderer {
 				if (!design.isNull("point")){
 					pt = Cast.toFloat(design.get("point"));
 				}
-				final int scale = 5;
-				BufferedImage _image = new BufferedImage((int)(_region.getWidth() * scale),
-						(int)(_region.getHeight() * scale),
-						BufferedImage.TYPE_INT_RGB);
-				Graphics g = _image.getGraphics();
-				g.setColor(Color.WHITE);
-				g.fillRect(0, 0, _image.getWidth(), _image.getHeight());
-				final int dpi = 72 * scale;
-				barcode.render(g, 0, 0, _image.getWidth(), _image.getHeight(), pt, dpi, code);
-				image = Image.getInstance(_image, Color.WHITE);
+				PdfTemplate tmp = cb.createTemplate(_region.getWidth(), _region.getHeight());
+				BarContent c = barcode.createContent(0, 0, (int)tmp.getWidth(), (int)tmp.getHeight(), pt, code);
+				tmp.setColorFill(Color.WHITE);
+				tmp.rectangle(0, 0, tmp.getWidth(), tmp.getHeight());
+				tmp.fill();
+				tmp.setColorFill(Color.BLACK);
+				List<Character> codes = new ArrayList<Character>();
+				for (String _code: barcode.encode(code)){
+					for (char _c: _code.toCharArray()){
+						codes.add(_c);
+					}
+				}
+				for (int i = 0; i < c.getBars().size(); i++){
+					BarContent.Bar b = c.getBars().get(i);
+					float y = tmp.getHeight() - b.getHeight();
+					char _type = codes.get(i);
+					if (_type == '3'){
+						y = tmp.getHeight() - c.getBars().get(0).getHeight();
+					}else if (_type == '4'){
+						y -= b.getHeight();
+					}
+					tmp.rectangle(b.getX(), y, b.getWidth(), b.getHeight());
+				}
+				tmp.fill();
+				image = Image.getInstance(tmp);
+				scaleMargin = 0.0f;
 			}else if (type != null && type.equals("itf")){
 				ITF barcode = new ITF();
 				if (Cast.toBool(design.get("without_text"))){
 					barcode.withText = false;
 				}
-				final int scale = 5;
-				BufferedImage _image = new BufferedImage((int)(_region.getWidth() * scale),
-						(int)(_region.getHeight() * scale),
+				PdfTemplate tmp = cb.createTemplate(_region.getWidth(), _region.getHeight());
+				BufferedImage _image = new BufferedImage((int)_region.getWidth(),
+						(int)_region.getHeight(),
 						BufferedImage.TYPE_INT_RGB);
 				Graphics g = _image.getGraphics();
-				g.setColor(Color.WHITE);
-				g.fillRect(0, 0, _image.getWidth(), _image.getHeight());
-				final int dpi = 72 * scale;
-				barcode.render(g, 0, 0, (int)_region.getWidth(), (int)_region.getHeight(), dpi, code);
-				image = Image.getInstance(_image, Color.WHITE);
+				BarContent c = barcode.createContent(g, 0, 0, (int)tmp.getWidth(), (int)tmp.getHeight(), code);
+				tmp.setColorFill(Color.WHITE);
+				tmp.rectangle(0, 0, tmp.getWidth(), tmp.getHeight());
+				tmp.fill();
+				tmp.setColorFill(Color.BLACK);
+				for (BarContent.Bar b: c.getBars()){
+					float y = tmp.getHeight() - b.getY() - b.getHeight();
+					tmp.rectangle(b.getX(), y, b.getWidth(), b.getHeight());
+				}
+				tmp.fill();
+				BarContent.Text t = c.getText();
+				if (t != null){
+					tmp.beginText();
+					Font f = FontFactory.getFont(t.getFont().getName(), BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+					tmp.setFontAndSize(f.getCalculatedBaseFont(true), t.getFont().getSize());
+					float y = tmp.getHeight() - t.getY() + (t.getFont().getSize() / 10);
+					tmp.showTextAligned(PdfContentByte.ALIGN_LEFT, t.getCode(), t.getX(), y, 0);
+					tmp.endText();
+				}
+				image = Image.getInstance(tmp);
+				scaleMargin = 0.0f;
 			}else{
 				BarcodeEAN barcode = new BarcodeEAN();
 				barcode.setCodeType(Barcode.EAN13);
@@ -170,7 +209,7 @@ public class BarcodeRenderer implements IElementRenderer {
 			}
 		}catch(Exception ex){}
 		if (image != null){
-			image.scaleAbsolute(_region.getWidth() - 2, _region.getHeight() - 2);
+			image.scaleAbsolute(_region.getWidth() - scaleMargin, _region.getHeight() - scaleMargin);
 			image.setAbsolutePosition(
 					renderer.trans.x(_region.left + 1),
 					renderer.trans.y(_region.bottom + 1));
