779a7758ef2f92c7b774f429f8fe9c891a9250bb
Shuetsu Ito <shuetsu@systembase.co.jp>
2015-10-21 12:02:05 +0900

    systembae.barcodeを更新 code128に対するテストを追加

--- src.test/test/Test_4_20_code128.java
+++ src.test/test/Test_4_20_code128.java
@@ -1,21 +1,18 @@
 package test;
 
 import java.io.FileOutputStream;
-import java.util.Date;
 
 import jp.co.systembase.report.Report;
 import jp.co.systembase.report.ReportPages;
 import jp.co.systembase.report.data.DummyDataSource;
 import jp.co.systembase.report.renderer.pdf.PdfRenderer;
 
-public class Test_4_20_barcode {
+public class Test_4_20_code128 {
 
 	public static void main(String[] args) throws Throwable {
-		String name = "test_4_20_barcode";
+		String name = "test_4_20_code128";
 		
 		Report report = new Report(ReadUtil.readJson("rrpt/" + name + ".rrpt"));
-		report.globalScope.put("time", new Date());
-        report.globalScope.put("lang", "java");
         report.fill(DummyDataSource.getInstance());
 		
 		ReportPages pages = report.getPages();
