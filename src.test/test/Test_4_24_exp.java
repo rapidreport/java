bdb8dca4ef630e4d6871394dd0cb5004a31f6d0f
ykoji <ykoji@systembase.co.jp>
2016-08-01 10:51:07 +0900

    クオートで囲んだ形式のメソッドパラメータのテスト追加

---
+++ src.test/test/Test_4_24_exp.java
@@ -0,0 +1,60 @@
+package test;
+
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+
+import jp.co.systembase.core.DataTable;
+import jp.co.systembase.report.Report;
+import jp.co.systembase.report.ReportPages;
+import jp.co.systembase.report.data.ReportDataSource;
+import jp.co.systembase.report.renderer.pdf.PdfRenderer;
+
+public class Test_4_24_exp {
+
+	public static void main(String[] args) throws Throwable {
+		String name = "test_4_24_exp";
+
+		Report report = new Report(ReadUtil.readJson("rrpt/" + name + ".rrpt"));
+		report.fill(new ReportDataSource(getDataTable()));
+
+		ReportPages pages = report.getPages();
+		{
+			FileOutputStream fos = new FileOutputStream("out/" + name + ".pdf");
+			try{
+				PdfRenderer renderer = new PdfRenderer(fos);
+				pages.render(renderer);
+			}finally{
+				fos.close();
+			}
+		}
+	}
+
+	private static DataTable getDataTable() throws Exception {
+		DataTable ret = new DataTable();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+
+		ret.setFieldNames("日本語", "半角 スペース", "全角　スペース","半角()","全角（）","円マーク\\","改行\n改行","シングルクォート\'",
+				"bumonCd","kaCd","kingaku(1)","kingaku(2)","kingaku(3)","bool(1)", "kingaku1", "kingaku2", "kingaku3");
+
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			10, 1, 1000, 10, 3, true, 2000, 20, 6);
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			10, 1, 990, 20, 4, false, 1980, 40, 8);
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			10, 2, 990, 20, 4, false, 1980, 40, 8);
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			10, 2, 990, 20, 4, false, 1980, 40, 8);
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			20, 1, 980, 20, 4, false, 1980, 40, 8);
+		ret.addRecord().puts("１番", sdf.parse("2013/03/01"),
+			"株式会社　岩手商事", "半角カッコ()", "全角カッコ（）", "円マーク\\", "改行\nした", "シングルクォート\'",
+			20, 1, 970, 20, 4, false, 1940, 40, 8);
+
+		return ret;
+	}
+}
