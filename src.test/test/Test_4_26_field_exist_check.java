93f586a57a1839511f7dd3dd15316c44e21ed13f
ykoji <ykoji@systembase.co.jp>
2016-10-06 10:01:50 +0900

    loggerを追加

--- src.test/test/Test_4_26_field_exist_check.java
+++ src.test/test/Test_4_26_field_exist_check.java
@@ -2,23 +2,27 @@ package test;
 
 import java.io.FileOutputStream;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
 import jp.co.systembase.core.DataTable;
+import jp.co.systembase.report.IReportLogger;
 import jp.co.systembase.report.Report;
 import jp.co.systembase.report.ReportPages;
+import jp.co.systembase.report.ReportSetting;
+import jp.co.systembase.report.component.ContentDesign;
+import jp.co.systembase.report.component.ElementDesign;
+import jp.co.systembase.report.component.EvalException;
 import jp.co.systembase.report.data.ReportDataSource;
 import jp.co.systembase.report.renderer.pdf.PdfRenderer;
-import jp.co.systembase.report.renderer.xls.XlsRenderer;
-import jp.co.systembase.report.renderer.xlsx.XlsxRenderer;
 
 public class Test_4_26_field_exist_check {
 
 	public static void main(String[] args) throws Throwable {
 		String name = "test_4_26_field_exist_check";
 
-		Report report = new Report(ReadUtil.readJson("rrpt/" + name + ".rrpt"));
+		ReportSetting setting = new ReportSetting();
+		Logger logger = new Logger();
+		setting.logger = logger;
+
+		Report report = new Report(ReadUtil.readJson("rrpt/" + name + ".rrpt"), setting);
 		report.fill(new ReportDataSource(getDataTable()));
 
 		ReportPages pages = report.getPages();
@@ -31,39 +35,24 @@ public class Test_4_26_field_exist_check {
 				fos.close();
 			}
 		}
-		{
-			FileOutputStream fos = new FileOutputStream("out/" + name + ".xls");
-			try{
-				HSSFWorkbook workBook = new HSSFWorkbook();
-				XlsRenderer renderer = new XlsRenderer(workBook);
-				renderer.newSheet(name);
-				pages.render(renderer);
-				workBook.write(fos);
-			}finally{
-				fos.close();
-			}
-		}
-		{
-			FileOutputStream fos = new FileOutputStream("out/" + name + ".xlsx");
-			try{
-				XSSFWorkbook workBook = new XSSFWorkbook();
-				XlsxRenderer renderer = new XlsxRenderer(workBook);
-				renderer.newSheet(name);
-				pages.render(renderer);
-				workBook.write(fos);
-			}finally{
-				fos.close();
-			}
-		}
 	}
 
 	private static DataTable getDataTable() throws Exception {
 		DataTable ret = new DataTable();
-
 		ret.setFieldNames("field1");
-
 		ret.addRecord().puts("正常に文字列が出力");
-
 		return ret;
 	}
+
+	public static class Logger implements IReportLogger {
+		@Override
+		public void evaluateError(String exp, EvalException ex) {
+			System.out.println("式：" + exp + ":" + ex.getClass().toString() + ":" + ex.getMessage());
+		}
+		@Override
+		public void elementRenderingError(ContentDesign contentDesign,
+				ElementDesign elementDesign, Throwable ex) {
+		}
+	}
+
 }
