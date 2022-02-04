3e942ef61d7c88afd6612f530859a50e7343cd43
shuetsu <shuetsu@systembase.co.jp>
2022-02-04 13:05:45 +0900

    numオペレータが数値をそのまま返すようにした。

--- src/jp/co/systembase/report/operator/NumOperator.java
+++ src/jp/co/systembase/report/operator/NumOperator.java
@@ -3,6 +3,7 @@ package jp.co.systembase.report.operator;
 import java.math.BigDecimal;
 import java.util.List;
 
+import jp.co.systembase.core.Cast;
 import jp.co.systembase.report.component.Evaluator;
 import jp.co.systembase.report.expression.IExpression;
 
@@ -13,7 +14,10 @@ public class NumOperator implements IOperator {
 			List<IExpression> params) throws Throwable {
 		evaluator.ValidateParamCount(params, 1);
 		Object o = evaluator.eval(params.get(0));
-		if (o instanceof String){
+		BigDecimal d = Cast.toBigDecimal(o);
+		if (d != null) {
+			return d;
+		}else if (o instanceof String){		
 			return new BigDecimal((String)o);
 		}else{
 			return null;
